672ad98551a08ab8ec36660dee0992e9f19c36a2
Kouame <kndri@redventures.com>
2021-03-13 23:29:22 -0500

    created card, logo, and icons component

--- App.js
+++ App.js
@@ -1,21 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { ScrollView, SafeAreaView } from "react-native";
+import styled from "styled-components";
+import Card from "./components/Card";
+import { Ionicons } from "@expo/vector-icons";
+import { NotificationIcon } from "./components/Icons";
+import Logo from "./components/Logo";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <SafeAreaView>
+        <ScrollView>
+          <TitleBar>
+            <Avatar source={require("./assets/avatar.jpg")} />
+            <Title>Welcome back,</Title>
+            <Name>Kouame</Name>
+            <NotificationIcon
+              style={{ position: "absolute", right: 20, top: 5 }}
+            />
+          </TitleBar>
+        </ScrollView>
+        <ScrollView
+          style={{
+            flexDirection: "row",
+            padding: 20,
+            paddingLeft: 12,
+            paddingTop: 30,
+          }}
+          horizontal={true}
+        >
+          <Logo image={require("./assets/logo-framerx.png")} text="Framer X" />
+          <Logo image={require("./assets/logo-figma.png")} text="Figma" />
+        </ScrollView>
+        <Subtitle>Continue learning</Subtitle>
+        <ScrollView
+          horizontal={true}
+          style={{ paddingBottom: 30 }}
+          showsHorizontalScrollIndicator={false}
+        >
+          <Card
+            title="Styled Components"
+            image={require("./assets/background2.jpg")}
+            caption="React Native"
+            logo={require("./assets/logo-react.png")}
+            subtitle="5 of 12 sections"
+          />
+          <Card
+            title="Styled Components"
+            image={require("./assets/background2.jpg")}
+            caption="React Native"
+            logo={require("./assets/logo-react.png")}
+            subtitle="5 of 12 sections"
+          />
+        </ScrollView>
+      </SafeAreaView>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 800;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 20px;
+  text-transform: uppercase;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
